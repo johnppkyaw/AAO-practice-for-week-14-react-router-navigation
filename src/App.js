e1d8cf3bb1da2bd1be476ee47fb92041ac2545f8
John <jjpyae@gmail.com>
2023-11-03 15:03:29 -0700

    practice complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
@@ -7,6 +8,20 @@ function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName='purple' activeStyle={{ fontWeight: 'bold' }} exact={true} to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName='purple' activeStyle={{ fontWeight: 'bold' }} exact={true} to='/stocks'>Stocks</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName='purple' activeStyle={{ fontWeight: 'bold' }} exact={true} to='/movies'>Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Switch>
         <Route exact path='/'>
           <Home />
@@ -16,6 +31,9 @@ function App() {
         </Route>
         <Route path='/movies'>
           <Movies />
+        </Route>
+        <Route path='/not-logged-in'>
+          <h1>You Must Be Logged In To Enter.</h1>
         </Route>
         <Route>
           <h1>Page Not Found</h1>
--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+
 function Stocks() {
+  let loggedIn = true;
+  let history = useHistory();
+  if(!loggedIn) {
+    return (
+      <Redirect to='/not-logged-in'></Redirect>
+    )
+  }
+
+  const handleClick = () => {
+    window.alert('Sending info to the DB!');
+    history.push('/');
+  };
+
   return (
     <div className='comp orange'>
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
